90b05c2d925d2094991793e2bb4b50508e0f948a
Sam Ostrowski <so31701@gmail.com>
2017-02-06 19:26:20 +0000

    test for heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express"),
-    routes = require("./app/routes/index.js"),
-    config = require("./config.js"),
-    mongoose = require("mongoose");
+    routes = require("./app/routes/index.js");
+    //config = require("./config.js"),
+    //mongoose = require("mongoose");
 
 var app = express();
 var port = process.env.PORT || 8080;
-var url = process.env.MONGOLAB_URI || "mongodb://localhost:27017/urlshortener";
+//var url = process.env.MONGOLAB_URI || "mongodb://localhost:27017/urlshortener";
 
-mongoose.connect(url);
+//mongoose.connect(url);
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
